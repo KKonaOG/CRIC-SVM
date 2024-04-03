5abad861714f26884605b569a41a5b85b983c22e
KKonaoG <38236274+KKonaOG@users.noreply.github.com>
2024-04-02 22:27:45 -0500

    Documentation and cleanuo

--- data_processing/data_conversion.py
+++ data_processing/data_conversion.py
@@ -1,87 +1,101 @@
-import numpy as np
-from sklearn import svm
-from scipy.io import loadmat
+'''
+This script is a one-off utility that takes in the raw 4D-RADAR tensors
+and converts them to db-Scale image representations of the tensor alongside
+reconstruction information in a specified directory. This is the primary
+automated stage of pre-processing. Following this stage, images are hand-cropped
+in order to a fully labelled dataset for the SVM.
+
+Author: Kyler Farrar (kaf386@msstate.edu)
+'''
+
+# Fundamental Requirements of the Conversion Process
 import os
-from multiprocessing import Pool
-from os import listdir
-import pandas as pd
-import gzip
-import json
-import time
+import numpy as np
+from scipy.io import loadmat
 import cv2
 import matplotlib.pyplot as plt
 
+# These are only required for multi-processing operations
+from multiprocessing import Pool
+import time
+
+'''
+data_directory is an individual datasequence from the K-Radar dataset.
+It should at the minimum contain the radar_tesseract and info_labels folders from
+the datasequence
+'''
 data_directory = "K:/dataset"
+
+'''
+converted_directory is the output location for the processed (converted) radar tensors
+in the form of .png files. These .png files are Range (Height) and Angle (Width) correlated.
+Importantly, elevation and doppler information have the intensity information averaged and act
+as a color normalization value in grayscale when saved. In order to re-obtain data mappings from
+this color mapping, minimum and maximum values of this axis are saved alongside the converted frames
+for use in reconstruction.
+'''
 converted_directory = "K:/dataset_simplified/radar_images"
 
 '''
-=============== STAGE 1: PRE-PROCESSING ===============
-In order to work on the dataset, we first must generate our features.
-What does this mean exactly? Well, we are provided a 4D RADAR Tensor
-and a set of labels classiying the data within those tensors. We should
-for each frame do two things:
-
-One: Sort the frame into a Train, Test, Verification grouping for later purposes (60/30/10, 50/40/10, etc.)
-Two:
-
-Using the bounding boxes provided by the labelled dataset, find the relevant slices of data within the
-frame. Until we have the dataset and know what data is available for the radar frame, it can be assumed we will be able
-to extract the following: Azimuth, Elevation, Range, and Doppler. These values may or may not need to be averaged for the
-boxed object. CRIC stands for Clustered Radar Intensity Classification which in this case can likely be boiled down to the
-avergae of the doppler return for the region of interest. So at the very least we will need that feature. 
-
-Stage 1 Pre-Processing should in the end result in three datasets (Train, Test, Verification) containing the feature data
-for the frames in those datasets, this will be fed into Stage 2 which handles the SVMs being fit to the feature sets.
-=======================================================
+info_arr.mat is obtained from the resources section of the K-Radar GitHub and
+contains a bin number to physical world vaue mapping for Range, Azimuth, and Elevation.
+Importantly, arr_doppler.mat is additionally available in the resources section,
+however is not required in this pre-processing stage.
 '''
-
-# We are provided these files by the resources section on the KRadar Github
-# Arr Doppler represent the Doppler Bins the RADAR outputs
-arrDoppler = loadmat("./arr_doppler.mat")["arr_doppler"]
-arrDopplerLookup = np.array(arrDoppler[0])
-
-# Arr Info, similar to Arr Doppler represents binning information, however
-# It contains range, elevation, and azimuth information. It is believed
-# that they acquired doppler by a secondary means and that is why it is split
-arrInfo = loadmat("./info_arr.mat")
-radar_range  = np.array(arrInfo["arrRange"][0])
-arrElevationLookup = np.array(arrInfo["arrElevation"][0])
-radar_azimuth = np.array(arrInfo["arrAzimuth"][0]*np.pi/180)
-
+radar_info = loadmat("./info_arr.mat") # Elevation is not needed in this case
+radar_range  = np.array(radar_info["arrRange"][0])
+radar_azimuth = np.array(radar_info["arrAzimuth"][0]*np.pi/180) # Azimuth is stored in degrees so we convert it back to radians
 
 '''
-By looking at info_arr.mat, we were able to find the dimensions of our tesseract arrays which represent the binned-data responses.
+    arrDREA's are the "code-name" assigned to RADAR tesseracts stored inside .mat files within the radar_tesseracts folder
+    of a datasequence. They are stored in order of Doppler -> Range -> Azimith -> Elevation. Thus arrDREA[0][0][0][0] would
+    return the intensity value for Doppler Bin 0, Range Bin 0, Azimuth Bin 0, and Elevation Bin 0.
 
-I.e the ranges are binned in X divisions, we can access what would be in Doppler Bin 1, Range Bin 1, Elevation Bin 1, and Azimuth Bin 1 by accesing arrDREA[0][0][0][0]
-It is up to us to decode those bins back into real numbers for our model. The real question is whether or not we want to flatten our array.
-
-I.e convert the 4-D array into a 2D array with rows being arbitrary and the other information just being encoded on the secondary axis
+    The actual process used in process_tesseract is a near exact duplicate of the process utilized inside K-Radar's GitHub
+    to convert the RADAR tesseracts to .bev files. An important difference is that they utilize it for data visualization
+    with bounding boxes, while, we use it for the purposes of obtaining data inputs for our models. 
+    (See: https://github.com/kaist-avelab/K-Radar/blob/c2eb194b5ba2e08177ddc59fb97510e3ebde2188/utils/util_dataset.py#L34)
 '''
-# Using the obtained Lookup information we will now begin to convert rows in tesseracts.
-# Tesseracts are the 4D radar's raw information
-# Array goes 4 deep: Doppler -> Range -> Elevation -> Azimuth -> Intensity (Actual value held by the array)
-# Doppler x Range x Elevation x Azimuth
 def process_tesseract(tesseract):
+
+    # Purely for logging purposes
     print("Beginning Conversion of: " + tesseract[:-4])
     start = time.time()
     
+    # Tesseract is the full file-path of the desired .mat file
     input_path = tesseract
+
+    # Set our output file to be in the converted directory folder with the same name as the original but instead of .mat, utilize .png
     output_path = os.path.join(converted_directory, os.path.basename(tesseract)[:-4] + ".png")
+
+    # Load the .mat file (this can take a minute and requires a good bit of RAM)
     arr_DREA = np.array(loadmat(input_path)['arrDREA'])
+
+    # Reorganize the tensor to be in: Doppler -> Range -> Elevation -> Azimuth Order
     radar_tensor = np.transpose(arr_DREA, (0, 1, 3, 2))
+
+    # Obtain Intensity means along the Doppler Axis and Elevation Axis
+    # This results in a Range by Angle Grid with Intensity means as the value
     radar_bev = np.mean(np.mean(radar_tensor, axis=0), axis=2)
 
     arr_0, arr_1 = np.meshgrid(radar_azimuth, radar_range)
     height, width = np.shape(radar_bev)
 
-
+    # Obtain Max and Min Values for the purposes of reconstruction file
     max_val = np.max(10*np.log10(radar_bev))
     min_val = np.min(10*np.log10(radar_bev))
 
+    # Save Reconstruction File
     with open(output_path[:-4] + ".txt", 'w') as min_max:
         min_max.writelines(str(max_val) + "\n")
         min_max.writelines(str(min_val))
 
+
+    # Draw the image file utilize grayscale normalization for the intensity values
+    # Importantly, the intensity values are scaled into dB values to avoid unnecessarily large
+    # values. This was nearly directly copied and pasted from K-Radar's function.
+
+    # TODO: Investigate usage of dpi=300 and its impacts.
     plt.clf()
     plt.cla()
     plt.pcolormesh(arr_0, arr_1, 10*np.log10(radar_bev), cmap='gray')
@@ -90,28 +104,40 @@ def process_tesseract(tesseract):
     plt.subplots_adjust(left = 0, bottom = 0, right = 1, top = 1, hspace = 0, wspace = 0)
     plt.savefig(output_path, bbox_inches='tight', pad_inches=0, dpi=300)
 
+    # Verify succesful image writing using OpenCV, importantly, CUBIC interpolation is
+    # utilized to map the data back to a more dense form if required.
     temp_img = cv2.imread(output_path)
     temp_row, temp_col, _ = temp_img.shape
     if not (temp_row == height and temp_col == width):
         temp_img_new = cv2.resize(temp_img, (width, height), interpolation=cv2.INTER_CUBIC)
         cv2.imwrite(output_path, temp_img_new)
 
+    # Finally, replot the data a last time without any axis information, etc.
+    # This is the last step of the conversion process and gives us a finalized
+    # converted image.
     plt.close()
     plt.pcolormesh(arr_0, arr_1, 10*np.log10(radar_bev), cmap='gray')
     plt.axis('off')
     plt.tight_layout()
     plt.savefig(output_path, dpi=300, bbox_inches='tight', pad_inches=0)
 
+    # Purely for logging purposes
     end = time.time()
     print("Conversion and saving of {} completed in {:.2f} seconds.".format(tesseract[:-4],end - start))
 
 def obtain_tesseract_paths(dataDirectory):
-    return [os.path.join(dataDirectory, "radar_tesseract", file) for file in listdir(os.path.join(dataDirectory, "radar_tesseract"))]
+    # Obtains the full directory path information for all .mat files in the radar_tesseract
+    # for the given data_directory
+    return [os.path.join(dataDirectory, "radar_tesseract", file) for file in os.listdir(os.path.join(dataDirectory, "radar_tesseract"))]
 
 
     
 if __name__ == "__main__":
+    # Obtains the full directory path information for all .mat files in the radar_tesseract
+    # for the given data_directory
     tesseractNames = obtain_tesseract_paths(data_directory)
+
+    # Utilizes 10 cores in a Pool to work on the tesseracts obtained by the obtain_tesseract_paths function
     with Pool(processes=10) as pool:
         pool.map(process_tesseract, tesseractNames)
     
--- data_processing/meta_explorer.py
+++ data_processing/meta_explorer.py
@@ -1,16 +1,90 @@
+'''
+This script is somewhat of a one-off utility that reads through the meta information
+provided in the K-Radar Google Drive. The meta folder's there contain the labelling information
+for the relevant data sequence. As such, we are able to examine the data distribution and label
+variety without requiring the entire K-Radar dataset.
+
+
+Author: Kyler Farrar (kaf386@msstate.edu)
+'''
+
 import os
 
+'''
+The directory which holds all the datasequence metas downloaded from the K-Radar Google Drive is the meta_directory. 
+It is assumed that the datasequence metas are unzipped in the following structure:
+
+meta_directory -
+    - 1_meta
+        - info_calib
+        - info_label
+        - time_info
+    - 2_meta
+        - info_calib
+        - info_label
+        - time_info
+    - 3_meta
+        - info_calib
+        - info_label
+        - time_info
+    - 4_meta
+        - info_calib
+        - info_label
+        - time_info
+    - 5_meta
+        - info_calib
+        - info_label
+        - time_info
+    .
+    .
+    .
+    - X_meta
+        - info_calib
+        - info_label
+        - time_info
+
+The only important folder used by the meta_explorer script is the info_label folder (which can be renamed as long as label_directory is reassigned) within each meta. All other folders within a meta are irrelevant.
+'''
 meta_directory = 'K:/datasequence_metas'
+
+# Label Directory is simply the name of the folder containing the labels inside each "meta folder"
 label_directory = 'info_label'
 
 def get_labels(directory):
+    # Reconstructs label base path
     label_directory_path = meta_directory + "/" + directory + "/" + label_directory
-    list_labels = os.listdir(label_directory_path)
-    print(list_labels)
 
+    # Obtains a list of the label files in the base path for that meta's labels
+    list_labels = os.listdir(label_directory_path)
+
+    # Dictionary of Labels and their Occurences
+    labels = {}
+
+    # Loop over each file in the list of labels for a meta
+    for label_file in list_labels:
+        with open(label_directory_path + "/" + label_file, 'r') as label:
+            label.readline() # Skip First Line as it is header informatio
+
+            # Look at each line in the file and select out the classification (should always be element 3 when you split by commas)
+            # If the classification exists in the dictionary, increase the number of occurences associated with it
+            # If the classification does not exist in the dictionary, create it and set its occurences to 1
+            for line in label:
+                key = str(line.split(',')[3].strip())
+                if (not labels.get(key)):
+                    labels[key] = 1
+                else:
+                    labels[key] = labels[key] + 1
+
+    # Print out the breakdown for the file for analysis
+    print("Meta: {}".format(directory))
+    print(labels)
+    print("======================")
 
 
 if __name__ == '__main__':
+    # Obtain list of directories within meta_directory
     list_metas = os.listdir(meta_directory)
+
+    # Obtain labels within each meta folder
     for meta in list_metas:
         get_labels(meta)
