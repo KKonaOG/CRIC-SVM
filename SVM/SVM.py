3939248d53d114f0d9a65a4f1865c7dd44bfd327
Andrew Thompson <99454471+AndrewThompson1087@users.noreply.github.com>
2024-04-13 10:34:43 -0500

    Update SVM.py to graph

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -1,13 +1,17 @@
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, f1_score, recall_score, precision_score, accuracy_score
-import seaborn as sns
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+import seaborn as sns
+from sklearn.metrics import confusion_matrix, f1_score, recall_score, precision_score, accuracy_score
 
+<<<<<<< HEAD
+=======
+#make dataset
+>>>>>>> a5bf33a (Update SVM.py to graph)
 kradar_dataset = pd.read_csv('kradar_processed.csv')
 kradar_dataset = kradar_dataset.drop(columns=['Dataset', 'Frame Number', 'Max Intensity', 'Min Intensity', 'X Size', 'Y Size'])
 
@@ -21,44 +25,47 @@ X_test_scaled = scaler.transform(X_test)
 # Train a multi-class SVM classifier
 # Possible changes: kernel (separate from Gp kernel), C parameter for regularization, gamma for , cache_size for speed that cost RAM
 # for GP, focus on C and gamma
+<<<<<<< HEAD
 
 svm_model = SVC(kernel='rbf', C=400, gamma = 'scale', decision_function_shape='ovo')
+=======
+svm_model = SVC(kernel='rbf', C=5, gamma = 'scale', decision_function_shape='ovo')
+>>>>>>> a5bf33a (Update SVM.py to graph)
 svm_model.fit(X_train_scaled, y_train)
 
-# # Create a mesh to plot the decision boundaries
-# x_min, x_max = X_train_scaled[:, 0].min() - 1, X_train_scaled[:, 0].max() + 1
-# y_min, y_max = X_train_scaled[:, 1].min() - 1, X_train_scaled[:, 1].max() + 1
-# xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.02),
-#                      np.arange(y_min, y_max, 0.02))
+# Create a mesh to plot the decision boundaries
+x_min, x_max = X_train_scaled[:, 0].min() - 1, X_train_scaled[:, 0].max() + 1
+y_min, y_max = X_train_scaled[:, 1].min() - 1, X_train_scaled[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.02),
+                     np.arange(y_min, y_max, 0.02))
 
-# Z = svm_model.predict(np.c_[xx.ravel(), yy.ravel()])
-# Z = Z.reshape(xx.shape)
+Z = svm_model.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
 
-# # Plot the decision boundaries
-# plt.contourf(xx, yy, Z, alpha=0.8, cmap=plt.cm.coolwarm)
-# plt.scatter(X_train_scaled[:, 0], X_train_scaled[:, 1], c=y_train, edgecolors='k', cmap=plt.cm.coolwarm)
-# plt.xlabel('Feature 1')
-# plt.ylabel('Feature 2')
-# plt.title('SVM Multi-Class Classification')
-# plt.show()
-
+# Get results
 y_predict = svm_model.predict(X_test_scaled)
 precision = precision_score(y_true=y_test, y_pred=y_predict, average='macro')
 accuracy = accuracy_score(y_true=y_test, y_pred=y_predict)
 recall = recall_score(y_true=y_test, y_pred=y_predict, average='macro')
 f1 = f1_score(y_true=y_test, y_pred=y_predict, average='macro')
-
-
 print("Accuracy: {}\nRecall: {}\nPrecision: {}\nF1: {}".format(accuracy, recall, precision, f1))
 
-cm = confusion_matrix(y_true=y_test, y_pred=y_predict)
+encoder = LabelEncoder()
+Z_encoded = encoder.fit_transform(Z.ravel())  # Flatten Z to fit
+Z_numeric = Z_encoded.reshape(Z.shape)  # Reshape back to the original shape of Z
+y_train_encoded = encoder.fit_transform(y_train)
+
+#Confusion Matrix
 plt.figure(figsize=(8, 6))
+cm = confusion_matrix(y_true=y_test, y_pred=y_predict)
 sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=svm_model.classes_, yticklabels=svm_model.classes_)
-plt.xlabel('Predicted')
-plt.ylabel('True')
-plt.title('Confusion Matrix for SVM Classifier')
+
+# Plot the decision boundaries
+plt.figure(figsize=(10, 8))
+plt.contourf(xx, yy, Z_numeric, alpha=0.8, cmap=plt.cm.coolwarm)
+plt.scatter(X_train_scaled[:, 0], X_train_scaled[:, 1], c=y_train_encoded, edgecolors='k', cmap=plt.cm.coolwarm)
+plt.xlabel('Avg Intensity')
+plt.ylabel('Total Size')
+plt.title('SVM Multi-Class Classification')
+plt.colorbar()
 plt.show()
-
-
-
-
